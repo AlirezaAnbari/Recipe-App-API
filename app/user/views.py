47836b6cf8b38fbaff3ab50339a784f8c081717c
Alireza Anbari <alireza.anbary@gmail.com>
2024-03-04 15:36:44 +0330

    authentication

--- app/user/views.py
+++ app/user/views.py
@@ -2,8 +2,14 @@
 Views for the user API
 """
 from rest_framework import generics
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
 
-from user.serializers import UserSerializer
+
+from user.serializers import (
+    UserSerializer,
+    AuthTokenSerializer,
+)
 
 
 class CreateUserView(generics.CreateAPIView):
